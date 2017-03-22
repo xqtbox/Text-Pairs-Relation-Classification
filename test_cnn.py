b861c60af90627d876decfed9d6d23616ee24ecd
chinawolfman@hotmail.com <chinawolfman@hotmail.com>
2017-03-22 19:13:20 +0800

    Little change.
    Little change.

--- test_cnn.py
+++ test_cnn.py
@@ -8,19 +8,19 @@ from text_cnn import TextCNN
 from tensorflow.contrib import learn
 
 BASE_DIR = randolph.cur_file_dir()
-model_file = BASE_DIR + "/runs/1489987989/checkpoints/output_graph.pb"
+model_file = BASE_DIR + "/runs/1490098338/checkpoints/output_graph.pb"
 
 # Data loading params
-tf.flags.DEFINE_string("training_data_file", BASE_DIR + '/Model1_Training.txt', "Data source for the training data.")
+tf.flags.DEFINE_string("validation_data_file", BASE_DIR + '/Model1_Validation.txt', "Data source for the validation data.")
 tf.flags.DEFINE_string("test_data_file", BASE_DIR + '/Model1_Test.txt', "Data source for the test data.")
-tf.flags.DEFINE_string("checkpoint_dir", BASE_DIR + '/runs/1489987989/checkpoints', "Checkpoint directory from training run")
+tf.flags.DEFINE_string("checkpoint_dir", BASE_DIR + '/runs/1490175368/checkpoints', "Checkpoint directory from training run")
 
 # Data parameters
-tf.flags.DEFINE_string("MAX_SEQUENCE_LENGTH", 120, "每个文本的最长选取长度(padding的统一长度),较短的文本可以设短些.")
-tf.flags.DEFINE_string("MAX_NB_WORDS", 5000, "整体词库字典中，词的多少，可以略微调大或调小.")
+tf.flags.DEFINE_string("MAX_SEQUENCE_LENGTH", 450, "每个文本的最长选取长度(padding的统一长度),较短的文本可以设短些.")
+tf.flags.DEFINE_string("MAX_NB_WORDS", 10000, "整体词库字典中，词的多少，可以略微调大或调小.")
 
 # Model Hyperparameters
-tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
+tf.flags.DEFINE_integer("embedding_dim", 300, "Dimensionality of character embedding (default: 128)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
@@ -33,6 +33,7 @@ tf.flags.DEFINE_integer("num_epochs", 10, "Number of training epochs (default: 2
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+tf.flags.DEFINE_boolean("gpu_options_allow_growth", True, "Allow gpu options growth")
 
 # Model export parameters
 tf.flags.DEFINE_string("input_graph_name", "input_graph.pb", "Graph input file of the graph to export")
@@ -54,13 +55,15 @@ vocab_size = data_helpers.load_vocab_size()
 pretrained_word2vec_matrix = data_helpers.load_word2vec_matrix(vocab_size, FLAGS.embedding_dim)
 
 checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+print(checkpoint_file)
+
 graph = tf.Graph()
 with graph.as_default():
 	session_conf = tf.ConfigProto(
 		allow_soft_placement=FLAGS.allow_soft_placement,
 		log_device_placement=FLAGS.log_device_placement)
+	session_conf.gpu_options.allow_growth = FLAGS.gpu_options_allow_growth
 	sess = tf.Session(config=session_conf)
-
 	with sess.as_default():
 		# Load the saved meta graph and restore variables
 		saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
@@ -80,43 +83,45 @@ with graph.as_default():
 		scores = graph.get_operation_by_name("output/scores").outputs
 		predictions = graph.get_operation_by_name("output/predictions").outputs[0]
 		softmaxScores = graph.get_operation_by_name("output/softmaxScores").outputs[0]
+		sigmoidScores = graph.get_operation_by_name("output/sigmoidScores").outputs[0]
 		topKPreds = graph.get_operation_by_name("output/topKPreds").outputs[0]
 
 		# Generate batches for one epoch
-		# batches = data_helpers.batch_iter(
-		# 	list(zip(x_test_front, x_test_behind)), FLAGS.batch_size, 1)
+		batches = data_helpers.batch_iter(list(zip(x_test_front, x_test_behind)), FLAGS.batch_size, 1, shuffle=False)
 
 		# Collect the predictions here
 		all_scores = []
 		all_softMaxScores = []
+		all_sigmoidScores = []
 		all_predictions = []
 		all_topKPreds = []
 
-		feed_dict = {
-			input_x_front: x_test_front,
-			input_x_behind: x_test_behind,
-			dropout_keep_prob: 1.0
-		}
+		for x_test_batch in batches:
+			x_batch_front, x_batch_behind = zip(*x_test_batch)
+			feed_dict = {
+				input_x_front: x_batch_front,
+				input_x_behind: x_batch_behind,
+				dropout_keep_prob: 1.0
+			}
+			batch_scores = sess.run(scores, feed_dict)
+			all_scores = np.append(all_scores, batch_scores)
 
-		batch_scores = sess.run(scores, feed_dict)
-		all_scores = np.append(all_scores, batch_scores)
+			batch_softMax_scores = sess.run(softmaxScores, feed_dict)
+			all_softMaxScores = np.append(all_softMaxScores, batch_softMax_scores)
 
-		batch_softMax_scores = sess.run(softmaxScores, feed_dict)
-		all_softMaxScores = np.append(all_softMaxScores, batch_softMax_scores)
+			batch_sigmoid_Scores = sess.run(sigmoidScores, feed_dict)
+			all_sigmoidScores = np.append(all_sigmoidScores, batch_sigmoid_Scores)
 
-		batch_predictions = sess.run(predictions, feed_dict)
-		all_predictions = np.concatenate([all_predictions, batch_predictions])
+			batch_predictions = sess.run(predictions, feed_dict)
+			all_predictions = np.concatenate([all_predictions, batch_predictions])
 
-		batch_topKPreds = sess.run(topKPreds, feed_dict)
-		all_topKPreds = np.append(all_topKPreds, batch_topKPreds)
+			batch_topKPreds = sess.run(topKPreds, feed_dict)
+			all_topKPreds = np.append(all_topKPreds, batch_topKPreds)
 
 		# all_softMaxScores    = np.append(all_softMaxScores, [[smxScore] for smxScore in batch_softMax_scores])
 
-print(all_scores)
-print(all_predictions[17])
-print(all_topKPreds[17])
+		np.savetxt('result11.txt', list(zip(all_predictions, all_topKPreds)), fmt='%s')
 
-np.savetxt('result.txt', list(zip(all_scores, all_predictions, all_topKPreds)), fmt='%s')
 # with open('result.txt', 'w') as fout:
 # 	for i in range(len(x_test_front)):
 # 		outStr = ''
