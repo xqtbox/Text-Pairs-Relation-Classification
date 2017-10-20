01130df96d2cef98e7f2eeaf20e913f9bb7c833d
RandolphVI <chinawolfman@hotmail.com>
2017-10-20 23:57:36 +0800

    Code refactoring.

--- text_cnn.py
+++ text_cnn.py
@@ -1,14 +1,15 @@
 # -*- coding:utf-8 -*-
 
 import tensorflow as tf
+from tensorflow.contrib.layers import batch_norm
 
 
-def linear(input_, output_size, scope=None):
+def linear(input_, output_size, name=None):
     """
     Linear map: output[k] = sum_i(Matrix[k, i] * args[i] ) + Bias[k]
     :param input_: a tensor or a list of 2D, batch x n, Tensors.
     :param output_size: int, second dimension of W[i].
-    :param scope: VariableScope for the created subgraph; defaults to "Linear".
+    :param name: VariableScope for the created subgraph; defaults to "Linear".
     :returns: A 2D Tensor with shape [batch x output_size] equal to \
                 sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
     :raises: ValueError, if some of the arguments has unspecified or wrong shape.
@@ -22,14 +23,14 @@ def linear(input_, output_size, scope=None):
     input_size = shape[1]
 
     # Now the computation.
-    with tf.variable_scope(scope or "SimpleLinear"):
-        matrix = tf.get_variable("Matrix", [output_size, input_size], dtype=input_.dtype)
-        bias_term = tf.get_variable("Bias", [output_size], dtype=input_.dtype)
+    with tf.name_scope(name or "SimpleLinear"):
+        W = tf.Variable(tf.truncated_normal(shape=[output_size, input_size], stddev=0.1), dtype=input_.dtype, name="W")
+        b = tf.Variable(tf.constant(0.1, shape=[output_size]), dtype=input_.dtype, name="b")
 
-    return tf.matmul(input_, tf.transpose(matrix)) + bias_term
+    return tf.nn.xw_plus_b(input_, W, b)
 
 
-def highway(input_, size, num_layers=1, bias=-2.0, f=tf.nn.relu, scope='Highway'):
+def highway(input_, size, num_layers=1, bias=-2.0, act=tf.nn.relu, name=None):
     """
     Highway Network (cf. http://arxiv.org/abs/1505.00387).
     t = sigmoid(Wy + b)
@@ -37,10 +38,10 @@ def highway(input_, size, num_layers=1, bias=-2.0, f=tf.nn.relu, scope='Highway'
     where g is nonlinearity, t is transform gate, and (1 - t) is carry gate.
     """
 
-    with tf.variable_scope(scope):
+    with tf.name_scope(name or "Highway"):
         for idx in range(num_layers):
-            g = f(linear(input_, size, scope='highway_lin_{}'.format(idx)))
-            t = tf.sigmoid(linear(input_, size, scope='highway_gate_{}'.format(idx)) + bias)
+            g = act(linear(input_, size, name='highway_lin_{}'.format(idx)))
+            t = tf.sigmoid(linear(input_, size, name='highway_gate_{}'.format(idx)) + bias)
             output = t * g + (1. - t) * input_
             input_ = output
 
@@ -48,13 +49,10 @@ def highway(input_, size, num_layers=1, bias=-2.0, f=tf.nn.relu, scope='Highway'
 
 
 class TextCNN(object):
-    """
-    A CNN for text classification.
-    Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
-    """
+    """A CNN for text classification."""
 
     def __init__(
-            self, sequence_length, num_classes, vocab_size, embedding_size,
+            self, sequence_length, num_classes, vocab_size, fc_hidden_size, embedding_size,
             embedding_type, filter_sizes, num_filters, l2_reg_lambda=0.0, pretrained_embedding=None):
 
         # Placeholders for input, output and dropout
@@ -62,104 +60,119 @@ class TextCNN(object):
         self.input_x_behind = tf.placeholder(tf.int32, [None, sequence_length], name="input_x_behind")
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+        self.is_training = tf.placeholder(tf.bool)
+
+        self.global_step = tf.Variable(0, trainable=False, name="Global_Step")
 
         # Keeping track of l2 regularization loss (optional)
         l2_loss = tf.constant(0.0)
 
-        # Vector = load_word2vec(TEXT_DATA_DIR, WORD2VEC_DIR, DICTIONARY_DIR, vocab_size, embedding_size)
-
         # Embedding layer
         with tf.device('/cpu:0'), tf.name_scope("embedding"):
-            # 原默认采用的是随机生成正态分布的词向量。
-            # Vector 是通过自己的语料库训练而得到的词向量。
-            # input_x_front 和 input_x_behind 共用词向量。
+            # 默认采用的是随机生成正态分布的词向量。
+            # 也可以是通过自己的语料库训练而得到的词向量。
             if pretrained_embedding is None:
-                self.W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name="W")
+                self.embedding = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+                                             name="embedding")
             else:
                 if embedding_type == 0:
-                    self.W = tf.constant(pretrained_embedding, name="W")
-                    self.W = tf.cast(self.W, tf.float32)
+                    self.embedding = tf.constant(pretrained_embedding, name="embedding")
+                    self.embedding = tf.cast(self.embedding, tf.float32)
                 if embedding_type == 1:
-                    self.W = tf.Variable(pretrained_embedding, name="W", trainable=True)
-                    self.W = tf.cast(self.W, tf.float32)
-            self.embedded_chars_front = tf.nn.embedding_lookup(self.W, self.input_x_front)
-            self.embedded_chars_behind = tf.nn.embedding_lookup(self.W, self.input_x_behind)
-            self.embedded_chars_expanded_front = tf.expand_dims(self.embedded_chars_front, -1)
-            self.embedded_chars_expanded_behind = tf.expand_dims(self.embedded_chars_behind, -1)
+                    self.embedding = tf.Variable(pretrained_embedding, name="embedding", trainable=True)
+                    self.embedding = tf.cast(self.embedding, tf.float32)
+            self.embedded_sentence_front = tf.nn.embedding_lookup(self.embedding, self.input_x_front)
+            self.embedded_sentence_behind = tf.nn.embedding_lookup(self.embedding, self.input_x_behind)
+            self.embedded_sentence_expanded_front = tf.expand_dims(self.embedded_sentence_front, -1)
+            self.embedded_sentence_expanded_behind = tf.expand_dims(self.embedded_sentence_behind, -1)
 
         # Create a convolution + maxpool layer for each filter size
         pooled_outputs_front = []
         pooled_outputs_behind = []
 
         for i, filter_size in enumerate(filter_sizes):
-            with tf.name_scope("conv-maxpool-{}".format(filter_size)):
+            with tf.name_scope("conv-filter{}".format(filter_size)):
                 # Convolution Layer
                 filter_shape = [filter_size, embedding_size, 1, num_filters]
-                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                W = tf.Variable(tf.truncated_normal(shape=filter_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), dtype=tf.float32, name="b")
                 conv_front = tf.nn.conv2d(
-                    self.embedded_chars_expanded_front,
+                    self.embedded_sentence_expanded_front,
                     W,
                     strides=[1, 1, 1, 1],
                     padding="VALID",
                     name="conv_front")
 
                 conv_behind = tf.nn.conv2d(
-                    self.embedded_chars_expanded_behind,
+                    self.embedded_sentence_expanded_behind,
                     W,
                     strides=[1, 1, 1, 1],
                     padding="VALID",
                     name="conv_behind")
 
+                # Batch Normalization Layer
+                conv_bn_front = batch_norm(tf.nn.bias_add(conv_front, b), is_training=self.is_training)
+                conv_bn_behind = batch_norm(tf.nn.bias_add(conv_behind, b), is_training=self.is_training)
+
                 # Apply nonlinearity
-                h_front = tf.nn.relu(tf.nn.bias_add(conv_front, b), name="relu_front")
-                h_behind = tf.nn.relu(tf.nn.bias_add(conv_behind, b), name="relu_behind")
+                conv_out_front = tf.nn.relu(conv_bn_front, name="relu_front")
+                conv_out_behind = tf.nn.relu(conv_bn_behind, name="relu_behind")
+
+            with tf.name_scope("pool-filter{}".format(filter_size)):
                 # Maxpooling over the outputs
                 pooled_front = tf.nn.max_pool(
-                    h_front,
+                    conv_out_front,
                     ksize=[1, sequence_length - filter_size + 1, 1, 1],
                     strides=[1, 1, 1, 1],
                     padding="VALID",
                     name="pool_front")
 
                 pooled_behind = tf.nn.max_pool(
-                    h_behind,
+                    conv_out_behind,
                     ksize=[1, sequence_length - filter_size + 1, 1, 1],
                     strides=[1, 1, 1, 1],
                     padding="VALID",
                     name="pool_behind")
 
-                pooled_outputs_front.append(pooled_front)
-                pooled_outputs_behind.append(pooled_behind)
+            pooled_outputs_front.append(pooled_front)
+            pooled_outputs_behind.append(pooled_behind)
 
         # Combine all the pooled features
         num_filters_total = num_filters * len(filter_sizes)
-        self.h_pool_front = tf.concat(pooled_outputs_front, 3)
-        self.h_pool_behind = tf.concat(pooled_outputs_behind, 3)
-        self.h_pool_flat_front = tf.reshape(self.h_pool_front, [-1, num_filters_total])
-        self.h_pool_flat_behind = tf.reshape(self.h_pool_behind, [-1, num_filters_total])
+        self.pool_front = tf.concat(pooled_outputs_front, 3)
+        self.pool_behind = tf.concat(pooled_outputs_behind, 3)
+        self.pool_flat_front = tf.reshape(self.pool_front, [-1, num_filters_total])
+        self.pool_flat_behind = tf.reshape(self.pool_behind, [-1, num_filters_total])
 
-        self.h_pool_flat_combine = tf.concat([self.h_pool_flat_front, self.h_pool_flat_behind], 1)
+        self.pool_flat_combine = tf.concat([self.pool_flat_front, self.pool_flat_behind], 1)
+
+        # Fully Connected Layer
+        with tf.name_scope("fc"):
+            W = tf.Variable(tf.truncated_normal(shape=[num_filters_total * 2, fc_hidden_size], stddev=0.1), name="W")
+            b = tf.Variable(tf.constant(0.1, shape=[fc_hidden_size]), dtype=tf.float32, name="b")
+            self.fc = tf.nn.xw_plus_b(self.pool_flat_combine, W, b)
+
+            # Batch Normalization Layer
+            self.fc_bn = batch_norm(self.fc, is_training=self.is_training)
+
+            # Apply nonlinearity
+            self.fc_out = tf.nn.relu(self.fc_bn, name="relu")
 
         # Add highway
-        with tf.name_scope("highway"):
-            self.h_highway = highway(self.h_pool_flat_combine, self.h_pool_flat_combine.get_shape()[1], 1, 0)
+        # with tf.name_scope("highway"):
+        #     self.h_highway = highway(self.pool_flat_combine, self.pool_flat_combine.get_shape()[1],
+        #                              num_layers=1, bias=0, name="Highway")
 
         # Add dropout
         with tf.name_scope("dropout"):
-            self.h_drop = tf.nn.dropout(self.h_highway, self.dropout_keep_prob)
+            self.h_drop = tf.nn.dropout(self.fc_out, self.dropout_keep_prob)
 
         # Final (unnormalized) scores and predictions
         with tf.name_scope("output"):
-            W = tf.get_variable(
-                "W",
-                shape=[num_filters_total * 2, num_classes],
-                initializer=tf.contrib.layers.xavier_initializer())
+            W = tf.Variable(tf.truncated_normal(shape=[fc_hidden_size, num_classes], stddev=0.1), name="W")
             b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-
             l2_loss += tf.nn.l2_loss(W)
             l2_loss += tf.nn.l2_loss(b)
-
             self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
             self.softmaxScores = tf.nn.softmax(self.scores, name="softmaxScores")
             self.predictions = tf.argmax(self.scores, 1, name="predictions")
